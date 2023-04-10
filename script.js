289056c1d3c198c685abb33aa44faaccc852a0f1
Gloria Schaffarczyk <gloria.schaffarczyk@gmail.com>
2023-04-10 22:58:13 +0200

    removed comment

--- script.js
+++ script.js
@@ -268,7 +268,7 @@ function createStatsChartData(statsData) {
     };
 }
 
-// CREATE TYPE OF CHART // BAR-CHART //
+// CREATE TYPE OF CHART //
 
 function createStatsChart(canvasElementId, data) {
     const ctxStats = document.getElementById(canvasElementId);
